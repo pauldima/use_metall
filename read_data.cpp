cb1454eec65759d7be03b48ee0b9023c42182f9e
Paul Dima <pauldima95@gmail.com>
2022-03-24 20:08:40 +0100

    Cleaned up and fixed pragma warning

--- read_data.cpp
+++ read_data.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <tuple>
 #include <boost/container/map.hpp>
 #include <boost/container/string.hpp>
 #include <metall/metall.hpp>
@@ -8,18 +9,17 @@ using alloc_type = metall::manager::allocator_type<T>;
 
 int main()
 {
-    using string = boost::container::string;
+    using boost_string = boost::container::string;
     using map_type = boost::container::map<int,
-                                           string,
+                                           boost_string,
                                            std::less<int>,
-                                           alloc_type<std::pair<const int, string>>>;
+                                           alloc_type<std::pair<const int, boost_string>>>;
 
-    const char *test_dir = "/tmp/metalltest";
-    metall::manager manager(metall::open_read_only, test_dir);
+    std::string dir("/tmp/metalltest");
+    metall::manager manager(metall::open_only, dir.c_str());
 
     map_type *map;
-    std::size_t n;
-    std::tie(map, n) = manager.find<map_type>("map");
+    std::tie(map, std::ignore) = manager.find<map_type>("map");
     std::cout << map->at(1) << " " << map->at(2) << std::endl;
 
     return 0;
--- write_data.cpp
+++ write_data.cpp
@@ -1,34 +1,25 @@
+#include <string>
 #include <boost/container/map.hpp>
 #include <boost/container/string.hpp>
-#include <metall/detail/file.hpp>
 #include <metall/metall.hpp>
-
 
 template <typename T>
 using alloc_type = metall::manager::allocator_type<T>;
 
-inline bool create_test_dir(const char* dir)
-{
-    if (!metall::mtlldetail::directory_exist(dir))
-        return metall::mtlldetail::create_directory(dir);
-    return true;
-}
-
 int main()
 {
-    using string = boost::container::string;
+    using boost_string = boost::container::string;
     using map_type = boost::container::map<int,
-                                           string,
+                                           boost_string,
                                            std::less<int>,
-                                           alloc_type<std::pair<const int, string>>>;
-    const char *test_dir = "/tmp/metalltest";
+                                           alloc_type<std::pair<const int, boost_string>>>;
+    std::string dir("/tmp/metalltest");
 
-    create_test_dir(test_dir);
-    metall::manager manager(metall::create_only, test_dir);
+    metall::manager manager(metall::create_only, dir.c_str());
     map_type *map = manager.construct<map_type>("map")(manager.get_allocator<>());
 
-    map->insert(std::move(std::pair<const int, string>(1, "Hello")));
-    map->insert(std::move(std::pair<const int, string>(2, "World")));
+    map->insert(std::move(std::pair<const int, boost_string>(1, "Hello")));
+    map->insert(std::move(std::pair<const int, boost_string>(2, "World")));
 
     return 0;
 }
