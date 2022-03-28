b0e2193c411c6f113d23f673ab605f476809b942
Paul Dima <pauldima95@gmail.com>
2022-03-29 00:26:45 +0200

    Fixed string storage, added pthreads package in CMakeLists

--- read_data.cpp
+++ read_data.cpp
@@ -9,17 +9,21 @@ using alloc_type = metall::manager::allocator_type<T>;
 
 int main()
 {
-    using boost_string = boost::container::string;
+    using boost_string = boost::container::basic_string<char, std::char_traits<char>, alloc_type<char>>;
+    using my_pair = std::pair<const int, boost_string>;
     using map_type = boost::container::map<int,
                                            boost_string,
                                            std::less<int>,
-                                           alloc_type<std::pair<const int, boost_string>>>;
+                                           alloc_type<my_pair>>;
 
     std::string dir("/tmp/metalltest");
     metall::manager manager(metall::open_only, dir.c_str());
 
     map_type *map;
     std::tie(map, std::ignore) = manager.find<map_type>("map");
+    if(map == nullptr) 
+        return 1;
+
     std::cout << map->at(1) << " " << map->at(2) << std::endl;
 
     return 0;
--- write_data.cpp
+++ write_data.cpp
@@ -8,18 +8,22 @@ using alloc_type = metall::manager::allocator_type<T>;
 
 int main()
 {
-    using boost_string = boost::container::string;
+    using boost_string = boost::container::basic_string<char, std::char_traits<char>, alloc_type<char>>;
+    using my_pair = std::pair<const int, boost_string>;
     using map_type = boost::container::map<int,
                                            boost_string,
                                            std::less<int>,
-                                           alloc_type<std::pair<const int, boost_string>>>;
+                                           alloc_type<my_pair>>;
     std::string dir("/tmp/metalltest");
 
     metall::manager manager(metall::create_only, dir.c_str());
-    map_type *map = manager.construct<map_type>("map")(manager.get_allocator<>());
+    auto allocator = manager.get_allocator<>();
+    map_type *map = manager.construct<map_type>("map")(allocator);
+    if(map == nullptr) 
+        return 1;
 
-    map->insert(std::move(std::pair<const int, boost_string>(1, "Hello")));
-    map->insert(std::move(std::pair<const int, boost_string>(2, "World")));
+    map->emplace(1, boost_string("Hellooooooooooooooooooooooooooooooooo", allocator));
+    map->emplace(2, boost_string("Worldddddddddddddddddddddddddddddddddd", allocator));
 
     return 0;
 }
